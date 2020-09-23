4f0f0303d21fea806ad42f98eccd32ca53f34ada
Daria Leichak <Daria_Leichak@epam.com>
2020-09-23 22:18:30 +0300

    add full_tittle helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return the custom title
+  def full_tittle(page_tittle = "")
+    base_tittle = "Ruby on Rails Sample App"
+    if page_tittle.empty?
+      base_tittle
+    else
+      #   "#{page_tittle} | #{base_tittle}"
+      page_tittle + " | " + base_tittle
+    end
+  end
 end
