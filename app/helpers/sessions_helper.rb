c14a03be12fb24eccedafd4cce907739260f7d20
Daria Leichak <Daria_Leichak@epam.com>
2020-10-01 15:12:39 +0300

    Finish log in/log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,13 +4,32 @@ module SessionsHelper
   def log_in(user)
     # put a cookies in the browser with encrypt
     session[:user_id] = user.id
+  end
 
+  # Remembers a user in a persistent sesson.
+  def remember(user)
+    user.remember
+    # cookies[:user_id] = { value: user.id,
+    #                       expires: 20.years.from_now.utc }
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   # Returns the current logged-in user (ia any)
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-    # @current_user = @curent_user || User.find_by(id: session[:user_id])
+    # logger.debug "session.user_id: #{session[:user_id]}"
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+      # @current_user = @curent_user || User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      # raise
+      # logger.debug "cookies.signed.user_id: #{user_id}"
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # Returns true if the user is logged in, false otherwais.
@@ -18,8 +37,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Forgets a user in a persistent sesson.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Logs out the current user.
-  def logouts
+  def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
